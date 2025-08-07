5ee08677019f0c27d28e83cee3c1a89e505cc28d
isaiassn7 <isaiassousa19092000@gmail.com>
2025-08-06 21:43:11 -0300

    completo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,49 +1,87 @@
-import { useEffect, useLayoutEffect } from 'react'
-import './style.css'
-import Trash from '../../assets/trash.svg'
-import api from '../../services/api'
+import { useEffect, useState, useRef } from "react";
+import "./style.css";
+import Trash from "../../assets/trash.svg";
+import api from "../../services/api";
 
 function Home() {
-  let users = []
+  const [users, setUsers] = useState([]);
 
-  async function getUsers(){
-    users = await api.get('/usuarios')
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  async function getUsers() {
+    const usersFromApi = await api.get("/usuarios");
+
+    setUsers(usersFromApi.data);
+  }
+
+  async function createUsers() {
+
+    await api.post("/usuarios", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value,
+    });
+
+    getUsers()
+  }
+
+  async function deleteUsers(id) {
+
+    await api.delete(`/usuarios/${id}`);
+
+    getUsers()
   }
 
   useEffect(() => {
-    getUsers()
-  }, [])
-
+    getUsers();
+  }, []);
 
   return (
     <>
-      <div className='container'>
+      <div className="container">
         <form>
           <h1>Cadastro de Usuários</h1>
-          <input placeholder='Nome' name='nome' type="text" />
-          <input placeholder='Idade' name='idade' type="number" />
-          <input placeholder='E-mail' name='email' type="email" />
-          <button type='button'>Cadastrar</button>
+          <input placeholder="Nome" name="nome" type="text" ref={inputName} />
+          <input
+            placeholder="Idade"
+            name="idade"
+            type="number"
+            ref={inputAge}
+          />
+          <input
+            placeholder="E-mail"
+            name="email"
+            type="email"
+            ref={inputEmail}
+          />
+          <button type="button" onClick={createUsers}>
+            Cadastrar
+          </button>
         </form>
 
-        {users.map((user) =>
-          <div key={user.id} className='card'>
-              <div>
-                <p>Nome:  <span>{user.name}</span></p>
-                <p>Idade: <span>{user.age}</span></p>
-                <p>Email: <span>{user.email}</span></p>
-              </div>
-              <button>
-                <img src={Trash} />
-              </button>
+        {users.map((user) => (
+          <div key={user.id} className="card">
+            <div>
+              <p>
+                Nome: <span>{user.name}</span>
+              </p>
+              <p>
+                Idade: <span>{user.age}</span>
+              </p>
+              <p>
+                Email: <span>{user.email}</span>
+              </p>
+            </div>
+            <button onClick={() => deleteUsers(user.id)}>
+              <img src={Trash} />
+            </button>
           </div>
-
-        )}
-
-
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
